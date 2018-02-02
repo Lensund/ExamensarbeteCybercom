270b856509ccfd749ad68ea54b3b1af55062f978
Mikael S <msjoestedt@gmail.com>
2018-02-02 17:13:51 +0100

    matlab code1

--- MatlabCode/Rostest.m
+++ MatlabCode/Rostest.m
@@ -1,12 +1,26 @@
 %Subscribe and fetch sum data
+%Install ROS for matlab
+%Install dependencies for custom ROS messages "Robotics System Toolbox
+%Interface for ROS Custom Message" 
+
 clc
 clear all
-rosinit
+%%
+rosinit         % Initiallize ROS
+%Only needs to be done ONCE
+driverpath = fullfile('catkin_ws','src', 'hrp'); % Specify path of custom drivers
+rosgenmsg(driverpath); % Load and generate custom msg packages
+%%
+
+EncoderMessage = rosmessage('am_driver/WheelEncoder');
+EncoderMessage1 = rossubscriber('/wheel_encoder');
+% Follow output of rosgenmsg
 X = [];
 Y = [];
-odomdata = rossubscriber('/odom');
+%odomdata = rossubscriber('/odom');
 
 pause(2);
+%% 
 t0 = clock;
 while etime(clock, t0) < 30 
     odom = receive(odomdata);
@@ -16,5 +30,5 @@ while etime(clock, t0) < 30
     Y = [Y, Ypose];
 end
 
-
+%%
 rosshutdown
