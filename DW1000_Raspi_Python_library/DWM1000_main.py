9586e88660f1a9b686800f4373c8de324ac5a22d
Mikael S <msjoestedt@gmail.com>
2018-03-07 15:31:40 +0100

    comment max speed

---
+++ DW1000_Raspi_Python_library/DWM1000_main.py
@@ -0,0 +1,38 @@
+#Dis tha main
+
+import DW1000
+import monotonic
+import DW1000Constants as C
+
+
+import DWM1000RangingAnchor_class
+
+
+
+DWM1000_module1 = DWM1000_ranging(module1, "82:17:5B:D5:A9:9A:E2:9C", 19, 16)
+#DWM1000_module2 = DWM1000_ranging(module2, uniqueID, SS, IRQ)
+#DWM1000_module3 = DWM1000_ranging(module3, uniqueID, SS, IRQ)
+#DWM1000_module4 = DWM1000_ranging(module4, uniqueID, SS, IRQ)
+
+def main():
+
+
+    range_module1 = DWM1000_module.loop()
+    #range_module2 = DWM1000_module.loop()
+    #range_module3 = DWM1000_module.loop()
+    #range_module4 = DWM1000_module.loop()
+
+
+
+
+
+
+    except KeyboardInterrupt:
+        DW1000.close()
+
+
+
+
+if __name__ == '__main__':
+
+    main()
