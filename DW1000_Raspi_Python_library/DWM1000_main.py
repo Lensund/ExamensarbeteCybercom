54227259f166a1d772bdc46f28d4d80d7d5c8ff2
Mikael S <msjoestedt@gmail.com>
2018-03-08 08:41:14 +0100

    new day new opportunities

--- DW1000_Raspi_Python_library/DWM1000_main.py
+++ DW1000_Raspi_Python_library/DWM1000_main.py
@@ -13,17 +13,19 @@ IRQ1 = 19
 CS2 = 20
 IRQ2 = 26
 
-#DWM1000_module1 = DWM1000_ranging("module1", "82:17:5B:D5:A9:9A:E2:9C", 16, 19)
-DWM1000_module2 = DWM1000_ranging("module2", "82:17:5B:D5:A9:9A:E2:9C", CS2, IRQ2)
-#DWM1000_module3 = DWM1000_ranging("module3", uniqueID, SS, IRQ)
-#DWM1000_module4 = DWM1000_ranging("module4", uniqueID, SS, IRQ)
+CS3 = 21
+IRQ3 = 13
+#DWM1000_module1 = DWM1000_ranging("module1", "82:17:5B:D5:A9:9A:E2:9C", CS1, IRQ1)
+#DWM1000_module2 = DWM1000_ranging("module2", "82:17:5B:D5:A9:9A:E2:9C", CS2, IRQ2)
+DWM1000_module3 = DWM1000_ranging("module3", "82:17:5B:D5:A9:9A:E2:9C", CS3, IRQ3)
+#DWM1000_module4 = DWM1000_ranging("module4", uniqueID, CS4, IRQ4)
 
 def das_loop():
     #Loop for calculating the range
     rangemodule1_old = None
     test = True
     while test:
-        range_module1 = DWM1000_module2.loop()
+        range_module1 = DWM1000_module3.loop()
         if range_module1 != None:
             #rangemodule1_old = range_module1
             test = False
