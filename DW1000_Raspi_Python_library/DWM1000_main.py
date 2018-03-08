ae6877a31ef2b2a0a5100187a304729181b738d4
Mikael S <msjoestedt@gmail.com>
2018-03-08 08:48:36 +0100

    new day new opportunities

--- DW1000_Raspi_Python_library/DWM1000_main.py
+++ DW1000_Raspi_Python_library/DWM1000_main.py
@@ -15,17 +15,23 @@ IRQ2 = 26
 
 CS3 = 21
 IRQ3 = 13
-#DWM1000_module1 = DWM1000_ranging("module1", "82:17:5B:D5:A9:9A:E2:9C", CS1, IRQ1)
-#DWM1000_module2 = DWM1000_ranging("module2", "82:17:5B:D5:A9:9A:E2:9C", CS2, IRQ2)
-DWM1000_module3 = DWM1000_ranging("module3", "82:17:5B:D5:A9:9A:E2:3A", CS3, IRQ3)
-#DWM1000_module4 = DWM1000_ranging("module4", uniqueID, CS4, IRQ4)
+
+CS4 = 12
+IRQ4 = 6
+#DWM1000_module1 = DWM1000_ranging("module1", "82:17:5B:D5:A9:9A:E2:1A", CS1, IRQ1)
+#DWM1000_module2 = DWM1000_ranging("module2", "82:17:5B:D5:A9:9A:E2:2A", CS2, IRQ2)
+#DWM1000_module3 = DWM1000_ranging("module3", "82:17:5B:D5:A9:9A:E2:3A", CS3, IRQ3)
+DWM1000_module4 = DWM1000_ranging("module4", "82:17:5B:D5:A9:9A:E2:4A", CS4, IRQ4)
+
+#Add 5th module if applied
+#DWM1000_module5 = DWM1000_ranging("module5", "82:17:5B:D5:A9:9A:E2:5A", CS5, IRQ5)
 
 def das_loop():
     #Loop for calculating the range
     rangemodule1_old = None
     test = True
     while test:
-        range_module1 = DWM1000_module3.loop()
+        range_module1 = DWM1000_module4.loop()
         if range_module1 != None:
             #rangemodule1_old = range_module1
             test = False
